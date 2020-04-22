cc2108ab6ec0eabacea3c15a7cc44a671285d050
maciexu <43970987+maciexu@users.noreply.github.com>
2020-04-22 00:03:50 -0400

    Create getting_started.py

---
+++ getting_started.py
@@ -0,0 +1,13 @@
+"""
+Importing Python modules
+
+Modules (sometimes called packages or libraries) help group together related sets of tools in Python. In this exercise, we'll examine two modules that are frequently used by Data Scientists:
+
+statsmodels: used in machine learning; usually aliased as sm
+seaborn: a visualization library; usually aliased as sns
+numpy: a module for performing mathematical operations on lists of data. 
+"""
+
+# tips:  Use * to represent the missing four letters
+
+
